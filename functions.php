ebe4567ab81d0647b8f05ccb2ba106d2fdfe0a44
Mel <239523369+melcasino@users.noreply.github.com>
2025-11-11 00:15:52 +0800

    Added theme setup functions
    All setup functions is within "inc/setup.php" file and the file is being loaded in "function.php" file.

--- functions.php
+++ functions.php
@@ -3,9 +3,11 @@
  * Theme functions file, which is autoloaded by WordPress. This file is used to
  * load any other necessary PHP files that will add functionality and bootstrap the theme.
  * 
+ * @package	  basse
  * @author    Mel Casiño
  * @copyright Copyright (c) 2025, Mel Casiño
  * @license   https://www.gnu.org/licenses/gpl-3.0.html GPL-3.0-or-late
+ * @since     0.1.0
  */
 
 
@@ -34,3 +36,12 @@ define( 'THEME_VER', wp_get_theme()->get( 'Version' ) );
 define( 'THEME_DIR', get_template_directory() );
 define( 'THEME_URI', esc_url( get_template_directory_uri() ) );
 define( 'THEME_NS', __NAMESPACE__ );
+
+
+
+/**
+ * Load required files
+ * 
+ */
+// Setup theme defaults and WordPress features
+require_once THEME_DIR . '/inc/setup.php';
---
+++ inc/setup.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Theme setup functions
+ * 
+ * @package	  basse
+ * @author    Mel Casiño
+ * @copyright Copyright (c) 2025, Mel Casiño
+ * @license   https://www.gnu.org/licenses/gpl-3.0.html GPL-3.0-or-late
+ * @since     0.1.0
+ */
+
+
+
+namespace basse;
+
+
+
+/**
+ * Exit if accessed directly
+ * 
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+
+	exit;
+
+}
+
+
+
+/**
+ * Setup theme defaults, register and remove WordPress features
+ * 
+ */
+function set_theme_defaults() {
+    // Enqueue editor styles.
+    add_editor_style(
+        array(
+            './assets/css/editor.css',
+        )
+    );
+
+    // Remove core block patterns.
+    remove_theme_support( 'core-block-patterns' );
+}
+add_action( 'after_setup_theme', THEME_NS . '\set_theme_defaults' );
+
+
+
+/**
+ * Register pattern categories
+ * 
+ */
+function register_pattern_categories() {
+    $block_pattern_categories = array(
+        'templates'	=> array(
+            'label' => __( 'Templates', THEME_NS ),
+        ),
+        'pages'		=> array(
+            'label' => __( 'Pages', THEME_NS ),
+        ),
+        'sections'	=> array(
+            'label' => __( 'Sections', THEME_NS ),
+        ),
+        'cards'	    => array(
+            'label' => __( 'Cards', THEME_NS ),
+        ),
+    );
+
+    foreach ( $block_pattern_categories as $name => $properties ) {
+        register_block_pattern_category( $name, $properties );
+    }
+}
+add_action( 'init', THEME_NS . '\register_pattern_categories', 9 );

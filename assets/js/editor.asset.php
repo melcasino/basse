08b420b00fa6b986e5ecaba984377b6d5ea900c9
Mel <239523369+melcasino@users.noreply.github.com>
2025-11-30 23:43:36 +0800

    Unregister blocks default style variations

--- assets/js/editor.asset.php
+++ assets/js/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '31d6cfe0d16ae931b73c');
+<?php return array('dependencies' => array('wp-blocks', 'wp-dom-ready'), 'version' => 'a638010b5326ca33b4a1');
---
+++ src/js/editor.js
@@ -0,0 +1,39 @@
+/**
+ * Primary Editor Script
+ * 
+ * @package	  basse
+ * @author    Mel Casiño
+ * @copyright Copyright (c) 2025, Mel Casiño
+ * @license   https://www.gnu.org/licenses/gpl-3.0.html GPL-3.0-or-late
+ * @since     0.1.0
+ */
+
+
+
+// Import all dependencies
+import { unregisterBlockStyle } from '@wordpress/blocks';
+import domReady from '@wordpress/dom-ready';
+
+
+
+// Unregister block stlye variations
+domReady( () => {
+    const blocks = [
+        {
+            'blockName': 'core/separator',
+            'styleVariations':  [ 'wide', 'dots' ],
+        },
+        {
+            'blockName': 'core/image',
+            'styleVariations':  [ 'rounded' ],
+        },
+        {
+            'blockName': 'core/quote',
+            'styleVariations':  [ 'plain' ],
+        }
+    ];
+
+    blocks.forEach( block => {
+        block.styleVariations.forEach( styleVariation => unregisterBlockStyle( block.blockName, styleVariation ) ); 
+    } );
+} );

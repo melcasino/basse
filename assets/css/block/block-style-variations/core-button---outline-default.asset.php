6e4783cece52e67508bf7f93959f5fde4e611cdd
Mel <239523369+melcasino@users.noreply.github.com>
2025-12-15 14:26:58 +0800

    Added and enqueued "core/button" style variations CSS

---
+++ assets/css/block/block-style-variations/core-button---outline-default.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '7d360568a09a6505bbac');
---
+++ assets/css/block/block-style-variations/core-button---outline-small.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'a9ba55d7d9c87a7c6fc1');

d89492b4c14cdf2f9f2a98b011f542a53949ba32
Mel Casiño <239523369+melcasino@users.noreply.github.com>
2026-03-14 17:16:46 +0800

    Create "template-home-grid" pattern

---
+++ patterns/template-home-grid.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: Home Template
+ * Slug: basse/template-home-grid
+ * Description: A three column grid layout for blog posts and archive pages.
+ * Categories: query
+ * Keywords: blog, posts, query, loop
+ * Block Types:
+ * Post Types:
+ * Template Types: front-page, home, index
+ * Viewport Width: 1920
+ * Inserter: false
+ *
+ * @package Basse
+ * @since Basse 0.1.0
+ */ 
+?>
+
+<!-- wp:template-part {"slug":"header","theme":"basse"} /-->
+<!-- wp:pattern {"slug":"basse/post-loop-grid-static-page-heading"} /-->
+<!-- wp:template-part {"slug":"footer","theme":"basse","style":{"spacing":{"margin":{"top":"0"}}}} /-->

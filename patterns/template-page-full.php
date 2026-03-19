bd393e020c0fba4af17b953090a3f596ea93b00b
Mel Casiño <239523369+melcasino@users.noreply.github.com>
2026-03-19 20:34:20 +0800

    Create "template-page-full" pattern

---
+++ patterns/template-page-full.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: Page Full
+ * Slug: basse/template-page-full
+ * Description: A full-width layout with no page title, perfect for building pages using patterns or from scratch.
+ * Categories: basse/templates
+ * Keywords: page, full-width
+ * Block Types:
+ * Post Types:
+ * Template Types: page
+ * Viewport Width: 1920
+ * Inserter: false
+ *
+ * @package Basse
+ * @since Basse 0.1.0
+ */ 
+?>
+
+<!-- wp:template-part {"slug":"header","theme":"basse","area":"header"} /-->
+
+<!-- wp:post-content {"tagName":"main","align":"wide","style":{"spacing":{"margin":{"top":"0"}}},"layout":{"type":"constrained"}} /-->
+
+<!-- wp:template-part {"slug":"footer","theme":"basse","area":"footer","style":{"spacing":{"margin":{"top":"0"}}}} /-->

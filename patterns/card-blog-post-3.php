65cd0fbeafbbe76fb14a1e07718bdf84d74f476c
Mel Casiño <239523369+melcasino@users.noreply.github.com>
2026-03-29 00:40:06 +0800

    Create "card-blog-post-3.php" pattern

---
+++ patterns/card-blog-post-3.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: Blog Post Card 3
+ * Slug: basse/blog-post-card-3
+ * Description: A blog post card with post title and excerpt. Use this within query block.
+ * Categories: query, basse/cards
+ * Keywords: card, blog post, search
+ * Block Types: core/query, core/post-template
+ * Post Types:
+ * Template Types:
+ * Viewport Width: 400
+ * Inserter: true
+ *
+ * @package Basse
+ * @since Basse 0.1.0
+ */ 
+?>
+
+<!-- wp:group {"tagName":"article","metadata":{"name":"Article"},"className":"basse-pattern-blog-post-3","style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"default"}} -->
+<article class="wp-block-group basse-pattern-blog-post-3">
+    <!-- wp:post-title {"fontSize":"medium"} /-->
+
+    <!-- wp:post-excerpt {"moreText":"Continue Reading","showMoreOnNewLine":false,"excerptLength":25} /-->
+</article>
+<!-- /wp:group -->

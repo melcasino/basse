b0fa1d0c781d7d675305e2e1f3cd5d2b7159e119
Mel Casiño <239523369+melcasino@users.noreply.github.com>
2026-03-29 01:52:33 +0800

    Create "template-search-results-list.php" pattern

---
+++ patterns/template-search-results-list.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: Seach Results List
+ * Slug: basse/template-search-results-list
+ * Description: The page that show search results in list format.
+ * Categories: basse/templates
+ * Keywords: search, search results, blog, posts, query, loop
+ * Block Types:
+ * Post Types:
+ * Template Types: search
+ * Viewport Width: 1920
+ * Inserter: false
+ *
+ * @package Basse
+ * @since Basse 0.1.0
+ */ 
+?>
+
+<!-- wp:template-part {"slug":"header","theme":"basse"} /-->
+
+<!-- wp:pattern {"slug":"basse/post-loop-list-search-results"} /-->
+
+<!-- wp:template-part {"slug":"footer","theme":"basse","style":{"spacing":{"margin":{"top":"0"}}}} /-->
